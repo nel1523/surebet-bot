82ebb2f4edd16dd0b0d600d3fd11c4f24d91db18
nel1523 <nel_15_23@hotmail.com>
2026-09-10 20:29:11 -0300

    Fetch Telegram bot info using API

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
-print("HOLA NELSON")
+import os
+import requests
+
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+
+respuesta = requests.get(
+    f"https://api.telegram.org/bot{TOKEN}/getMe",
+    timeout=10
+)
+
+print(respuesta.json())
